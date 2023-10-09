ff15749485a25f73ff7e7214b5e8970172a35853
Matthew Ross <mer148@pitt.edu>
2023-10-09 16:11:54 -0400

    User chooses difficulty mode

--- app/wortle.hs
+++ app/wortle.hs
@@ -6,8 +6,7 @@ import System.Random ( getStdGen, Random(randomR), StdGen )
 import Control.Monad ( when )
 
 
---- TODO: 
----       Allow user to choose easy/hard mode (if the guess has to be a word)
+--- TODO: Allow user to choose easy/hard mode (if the guess has to be a word)
 main :: IO ()
 main = do
     putStrLn "Wilkommen zu Wortle"
@@ -31,9 +30,11 @@ preGameInstructions gen = do
     wordLength <- prompt "Möchten Sie ein Wort mit einer bestimmeten Länge? Wenn ja, wähl eine Zahl.\n"
     let winnowed_word_list = filterWordList word_list wordLength
     guessAmt <- prompt "Wie viele Versuche solle ich Ihnen geben? Der Standard ist 5.\n"
-    if not (null guessAmt) && all (`elem` "1234567890") guessAmt 
-        then pickSecretWord winnowed_word_list gen (read guessAmt) 
-        else pickSecretWord winnowed_word_list gen 5
+    diffMode <- prompt "Und letztens, möchten Sie das Spiel im leichten oder im schweren modus spielen?\nIm schweren Modus, muss alle Ihre Verusche echte Wörter sein. Der Standard ist schweren Modus.\n1)Leicht\n2)Schwer\n"
+    if not (null guessAmt) && all (`elem` "1234567890") guessAmt then 
+        pickSecretWord winnowed_word_list gen (read guessAmt) (diffMode == "1")
+        else pickSecretWord winnowed_word_list gen 5 (diffMode == "1")
+    
 
 
 prompt :: String -> IO String
@@ -43,15 +44,15 @@ prompt str = do
     getLine
 
 
-pickSecretWord :: [[Char]] -> StdGen -> Int -> IO ()
-pickSecretWord words gen guesses = do
+pickSecretWord :: [[Char]] -> StdGen -> Int -> Bool -> IO ()
+pickSecretWord words gen guesses hardMode = do
     let (index, newGen) = randomR (0, length words - 1) gen :: (Int, StdGen)
     putStrLn ("Das Wort hat " ++ show (length (words !! index)) ++ " Buchstaben.")
-    getGuess guesses (words !! index) newGen words 
+    getGuess guesses (words !! index) newGen words hardMode
 
 
-getGuess :: (Eq t, Num t, Show t) => t -> [Char] -> StdGen -> [[Char]] -> IO ()
-getGuess count word nextGen words
+getGuess :: (Eq t, Num t, Show t) => t -> [Char] -> StdGen -> [[Char]] -> Bool -> IO ()
+getGuess count word nextGen words hardMode
     | count == 0 = do
         putStrLn ("Sie haben verloren... Das Wort ist " ++ word ++ ".")
         askCont words nextGen
@@ -62,14 +63,17 @@ getGuess count word nextGen words
             askCont words nextGen
         else if length guess > length word then do
             putStrLn "Das Wort ist nicht so lang! Versuch es wieder."
-            getGuess count word nextGen words
+            getGuess count word nextGen words hardMode
         else if length guess < length word then do
             putStrLn "Das Wort ist noch länger! Versuch es wieder."
-            getGuess count word nextGen words
+            getGuess count word nextGen words hardMode
+        else if hardMode && notElem guess words then do
+            putStrLn "Das ist kein Wort, das ich kenne! Versuch es wieder."
+            getGuess count word nextGen words hardMode
         else do
             let clue = genHint word 0 guess
             putStrLn ("    " ++ clue)
-            getGuess (count-1) word nextGen words
+            getGuess (count-1) word nextGen words hardMode
 
 
 genHint :: [Char] -> Int -> [Char] -> [Char]
